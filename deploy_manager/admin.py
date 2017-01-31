ebcd8afded1b5cdfd8113c2fc65b78f5d481ef6a
吴文豪 <wwh529280602@live.cn>
2017-01-31 13:44:38 +0800

    业务模块：上级业务模块的过滤太多了，应该把没有子节点的数据过滤掉

--- deploy_manager/admin.py
+++ deploy_manager/admin.py
@@ -19,11 +19,31 @@ from saltjob.salt_token_id import token_id
 from saltops.settings import SALT_REST_URL
 
 
+class ProjectModuleFilter(admin.SimpleListFilter):
+    title = '上级业务模块'
+    parameter_name = 'parent'
+
+    def lookups(self, request, model_admin):
+        rs = set([c.parent for c in ProjectModule.objects.all()])
+        v = set()
+        for obj in rs:
+            if obj is not None:
+                v.add((obj.id, obj.name))
+        return v
+
+    def queryset(self, request, queryset):
+        if 'parent' in request.GET:
+            parentid = request.GET['parent']
+            return queryset.filter(parent=parentid)
+        else:
+            return queryset.all()
+
+
 @admin.register(ProjectModule)
 class ProjectModuleAdmin(MPTTModelAdmin):
     list_display = ['name', 'parent', 'create_time', 'update_time']
     search_fields = ['name']
-    list_filter = ['parent']
+    list_filter = [ProjectModuleFilter, ]
 
 
 class ProjectVersionInline(admin.TabularInline):
@@ -89,7 +109,7 @@ class ProjectAdmin(admin.ModelAdmin):
     deploydefaultAction.short_description = "部署默认版本"
 
     # class Media:
-        # js = ('/static/js/Project.js',)
+    # js = ('/static/js/Project.js',)
 
 
 class DeployJobDetailInline(admin.StackedInline):
