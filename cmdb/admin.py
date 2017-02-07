8fb6b35632ffa809e6dfa6022b8a8980a423a7d5
kira <529280602@qq.com>
2017-02-07 16:53:27 +0800

    CMDB代码优化

--- cmdb/admin.py
+++ cmdb/admin.py
@@ -97,7 +97,6 @@ class IDCAdmin(NestedModelAdmin):
     def cabinet_count(self, obj):
         return '<a href="/admin/cmdb/cabinet/?q=&idc=%s">%s</a>' % (obj.id, obj.cabinet_set.count())
 
-
     cabinet_count.short_description = '机柜数量'
     cabinet_count.allow_tags = True
 
@@ -114,8 +113,28 @@ class ISPAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class CabinetFilter(admin.SimpleListFilter):
+    title = '机柜'
+    parameter_name = 'cabinet'
+
+    def lookups(self, request, model_admin):
+        rs = set([c for c in Cabinet.objects.all()])
+        v = set()
+        for obj in rs:
+            if obj is not None:
+                v.add((obj.id, obj.name))
+        return v
+
+    def queryset(self, request, queryset):
+        if 'cabinet' in request.GET:
+            cabinet = request.GET['cabinet']
+            return queryset.filter(cabinet=cabinet)
+        else:
+            return queryset.all()
+
+
 @admin.register(Rack)
 class RackAdmin(admin.ModelAdmin):
     list_display = ['cabinet', 'name', 'create_time', 'update_time']
     search_fields = ['cabinet', 'name']
-    list_filter = ['cabinet']
+    list_filter = [CabinetFilter]
