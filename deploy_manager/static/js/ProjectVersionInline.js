c63f2a5d7ff3e896fccfff01c865a1d7b3ac0495
kira <529280602@qq.com>
2017-02-16 15:54:48 +0800

    修复业务部署默认选中的问题

--- deploy_manager/static/js/ProjectVersionInline.js
+++ deploy_manager/static/js/ProjectVersionInline.js
@@ -12,28 +12,5 @@
                 }
             });
         }
-        // $('table tr').each(function () {
-        //     if (i != 0 && this.children.length > 2) {
-        //         $(this.children[2].children[0]).on('click', function () {
-        //             var j = 0;
-        //             var _this = this;
-        //             $('table tr').each(function () {
-        //                 if (j != 0 && this.children.length > 2) {
-        //                     if ($(this.children[2].children[0])[0] != _this) {
-        //                         $(this.children[2].children[0]).removeAttr("checked")
-        //                     }
-        //                 }
-        //                 j++
-        //             });
-        //         });
-        //     }
-        //     i++;
-        // });
-
-        // var addNewBtn = $(".add-row")[0].children[0].children[0]
-        // $(addNewBtn).on('click', function () {
-        //     $("input[type='checkbox']").removeAttr("checked");
-        // });
-
     });
 })(django.jQuery);
