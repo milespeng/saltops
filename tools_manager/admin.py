bcc75f76d8fbeb8c25fa0768e7502f2b648e618b
kira <529280602@qq.com>
2017-02-16 13:11:36 +0800

    优化任务执行模板

--- tools_manager/admin.py
+++ tools_manager/admin.py
@@ -31,6 +31,7 @@ class ToolsScriptAdmin(admin.ModelAdmin):
     def change_view(self, request, object_id, form_url='', extra_context=None):
         extra_context = extra_context or {}
         extra_context['hostList'] = Host.objects.all()
+        extra_context['is_edit'] = True
         return super(ToolsScriptAdmin, self).change_view(request, object_id=object_id, form_url=form_url,
                                                          extra_context=extra_context)
 
