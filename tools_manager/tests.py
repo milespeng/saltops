21144f023c8f78ae591f71f0c922c9e5c16a2151
kira <529280602@qq.com>
2017-03-07 20:19:37 +0800

    开始添加单元测试

--- tools_manager/tests.py
+++ tools_manager/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from cmdb.models import Host
+from saltjob.tasks import execTools
+from tools_manager.models import *
+
+TARGET_HOST = "ubuntu"
+
+
+class ApacheModuleTest(TestCase):
+    def setUp(self):
+        try:
+            tool_type = ToolsTypes.objects.create(name='测试模块')
+            ToolsScript.objects.create(name='demo', tool_script='apache.version', tools_type=tool_type, tool_run_type=4)
+            Host.objects.create(host_name=TARGET_HOST)
+        except Exception as e:
+            print(e)
+
+    def test_apache_version(self):
+        obj = ToolsScript.objects.get(name='demo')
+        host = Host.objects.get(host_name=TARGET_HOST)
+        list = []
+        list.append(host.id)
+        execTools(obj, list, "")
