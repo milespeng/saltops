9cdb841b2744d0385617143c12643b08bba139cb
kira <529280602@qq.com>
2017-04-07 23:53:15 +0800

    使用DjangoForm优化表单处理模式

--- cmdb/c_views/isp.py
+++ cmdb/c_views/isp.py
@@ -1,13 +1,22 @@
 from http.client import HTTPResponse
 
 from django.db.models import Model
-from django.forms import ModelForm
+from django.forms import *
 from django.shortcuts import render, render_to_response, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.template import RequestContext
 
 from cmdb.models import ISP
 from common.pageutil import preparePage
+
+
+class ISPForm(ModelForm):
+    class Meta:
+        model = ISP
+        fields = ('__all__')
+        widgets = {
+            'name': TextInput({'class': 'form-control'})
+        }
 
 
 def isp_list(request):
@@ -32,29 +41,24 @@ def isp_delete_entity(request):
 def isp_add(request):
     title = '新增ISP'
     action = '/frontend/cmdb/isp_list/isp_add_action/'
+    form = ISPForm()
     return render(request, 'frontend/cmdb/isp_form.html', locals())
 
 
 def isp_add_action(request):
-    obj = ISP(
-        name=request.POST.get('name', ''),
-    )
-    obj.save()
-
+    form = ISPForm(request.POST)
+    form.save()
     return redirect('/frontend/cmdb/isp_list/')
 
 
 def isp_edit(request, pk):
     title = '编辑ISP'
     action = '/frontend/cmdb/isp_list/%s/isp_edit_action/' % pk
-    entity = ISP.objects.get(pk=pk)
+    form = ISPForm(instance=ISP.objects.get(pk=pk))
     return render(request, 'frontend/cmdb/isp_form.html', locals())
 
 
 def isp_edit_action(request, pk):
-    obj = ISP.objects.get(pk=pk)
-    obj.name = request.POST.get('name', '')
-    obj.comment = request.POST.get('comment', '')
-    obj.save()
-
+    form = ISPForm(request.POST, instance=ISP.objects.get(pk=pk))
+    form.save()
     return redirect('/frontend/cmdb/isp_list/')
