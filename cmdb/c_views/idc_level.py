d8190d6d6d56cbdcdb9f18ab9bcd992ac6d7ec77
kira <529280602@qq.com>
2017-04-09 15:52:05 +0800

    机房等级代码优化

--- cmdb/c_views/idc_level.py
+++ cmdb/c_views/idc_level.py
@@ -1,11 +1,22 @@
 from http.client import HTTPResponse
 
+from django.forms import *
 from django.shortcuts import render, render_to_response, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.template import RequestContext
 
 from cmdb.models import IDCLevel
 from common.pageutil import preparePage
+
+
+class IDCLevelForm(ModelForm):
+    class Meta:
+        model = IDCLevel
+        fields = '__all__'
+        widgets = {
+            'name': TextInput({'class': 'form-control'}),
+            'comment': Textarea({'class': 'form-control'})
+        }
 
 
 def idc_level_list(request):
@@ -27,32 +38,32 @@ def idc_level_delete_entity(request):
 
 
 def idc_level_add(request):
+    form = IDCLevelForm()
     title = '新增机房等级'
     action = '/frontend/cmdb/idc_level_list/idc_level_add_action/'
     return render(request, 'frontend/cmdb/idc_level_form.html', locals())
 
 
 def idc_level_add_action(request):
-    obj = IDCLevel(
-        name=request.POST.get('name', ''),
-        comment=request.POST.get('comment', ''),
-    )
-    obj.save()
-
-    return redirect('/frontend/cmdb/idc_level_list/')
+    form = IDCLevelForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('/frontend/cmdb/idc_level_list/')
+    else:
+        return render(request, 'frontend/cmdb/idc_level_form.html', locals())
 
 
 def idc_level_edit(request, pk):
     title = '编辑机房等级'
     action = '/frontend/cmdb/idc_level_list/%s/idc_level_edit_action/' % pk
-    entity = IDCLevel.objects.get(pk=pk)
+    form = IDCLevelForm(instance=IDCLevel.objects.get(pk=pk))
     return render(request, 'frontend/cmdb/idc_level_form.html', locals())
 
 
 def idc_level_edit_action(request, pk):
-    obj = IDCLevel.objects.get(pk=pk)
-    obj.name = request.POST.get('name', '')
-    obj.comment = request.POST.get('comment', '')
-    obj.save()
-
-    return redirect('/frontend/cmdb/idc_level_list/')
+    form = IDCLevelForm(request.POST, instance=IDCLevel.objects.get(pk=pk))
+    if form.is_valid():
+        form.save()
+        return redirect('/frontend/cmdb/idc_level_list/')
+    else:
+        return render(request, 'frontend/cmdb/idc_level_form.html', locals())
