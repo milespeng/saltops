f504f7b8bf50edb405669618e917dd22f1cdf75d
kira <529280602@qq.com>
2017-04-22 23:12:14 +0800

    基础模块改造

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -15,15 +15,17 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'IDCLevel',
-            'list_url': '/frontend/cmdb/idc_level_list/',
-            'form_template_path': 'frontend/common/basic_form.html',
-            'template_path': 'frontend/cmdb/idc_level_list.html',
-            'add_title': '新增机房等级',
-            'add_action': '/frontend/cmdb/idc_level_list/idc_level_add_action/',
-            'edit_title': '编辑机房等级',
-            'edit_action': '/frontend/cmdb/idc_level_list/%s/idc_level_edit_action/',
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'IDCLevel',
+                'list_url': '/frontend/cmdb/idc_level_list/',
+                'form_template_path': 'frontend/common/basic_form.html',
+                'template_path': 'frontend/cmdb/idc_level_list.html',
+                'add_title': '新增机房等级',
+                'add_action': '/frontend/cmdb/idc_level_list/idc_level_add_action/',
+                'edit_title': '编辑机房等级',
+                'edit_action': '/frontend/cmdb/idc_level_list/%s/idc_level_edit_action/',
+            }
         }),
 
     # ISP
@@ -36,16 +38,17 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'ISP',
-            'list_url': '/frontend/cmdb/isp_list/',
-            'form_template_path': 'frontend/common/basic_form.html',
-            'template_path': 'frontend/cmdb/isp_list.html',
-            'add_title': '新增ISP',
-            'add_action': '/frontend/cmdb/isp_list/isp_add_action/',
-            'edit_title': '编辑ISP',
-            'edit_action': '/frontend/cmdb/isp_list/%s/isp_edit_action/',
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'ISP',
+                'list_url': '/frontend/cmdb/isp_list/',
+                'form_template_path': 'frontend/common/basic_form.html',
+                'template_path': 'frontend/cmdb/isp_list.html',
+                'add_title': '新增ISP',
+                'add_action': '/frontend/cmdb/isp_list/isp_add_action/',
+                'edit_title': '编辑ISP',
+                'edit_action': '/frontend/cmdb/isp_list/%s/isp_edit_action/',
+            }}),
 
     # IDC
     url(r'^idc_list/', include([
@@ -57,17 +60,18 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'IDC',
-            'list_url': '/frontend/cmdb/idc_list/',
-            'form_template_path': 'frontend/common/basic_form.html',
-            'template_path': 'frontend/cmdb/idc_list.html',
-            'add_title': '新增机房',
-            'add_action': '/frontend/cmdb/idc_list/idc_add_action/',
-            'edit_title': '编辑机房',
-            'edit_action': '/frontend/cmdb/idc_list/%s/idc_edit_action/',
-            'plugin_name': 'idc_list_plugin'
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'IDC',
+                'list_url': '/frontend/cmdb/idc_list/',
+                'form_template_path': 'frontend/common/basic_form.html',
+                'template_path': 'frontend/cmdb/idc_list.html',
+                'add_title': '新增机房',
+                'add_action': '/frontend/cmdb/idc_list/idc_add_action/',
+                'edit_title': '编辑机房',
+                'edit_action': '/frontend/cmdb/idc_list/%s/idc_edit_action/',
+                'plugin_name': 'idc_list_plugin'
+            }}),
 
     # 机柜
     url(r'^cabinet_list/', include([
@@ -79,17 +83,18 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'Cabinet',
-            'list_url': '/frontend/cmdb/cabinet_list/',
-            'form_template_path': 'frontend/common/basic_form.html',
-            'template_path': 'frontend/cmdb/cabinet_list.html',
-            'add_title': '新增机柜',
-            'add_action': '/frontend/cmdb/cabinet_list/cabinet_add_action/',
-            'edit_title': '编辑机柜',
-            'edit_action': '/frontend/cmdb/cabinet_list/%s/cabinet_edit_action/',
-            'plugin_name': 'idc_list_plugin'
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'Cabinet',
+                'list_url': '/frontend/cmdb/cabinet_list/',
+                'form_template_path': 'frontend/common/basic_form.html',
+                'template_path': 'frontend/cmdb/cabinet_list.html',
+                'add_title': '新增机柜',
+                'add_action': '/frontend/cmdb/cabinet_list/cabinet_add_action/',
+                'edit_title': '编辑机柜',
+                'edit_action': '/frontend/cmdb/cabinet_list/%s/cabinet_edit_action/',
+                'plugin_name': 'idc_list_plugin'
+            }}),
 
     url(r'^rack_list/(?P<idc_id>\d+)/(?P<cabinet_id>\d+)/load_rack_list/', load_rack_list),
     url(r'^rack_list/(?P<pk>\d+)/load_cabinet_list/', load_cabinet_list),
@@ -103,19 +108,20 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'Rack',
-            'list_url': '/frontend/cmdb/rack_list/',
-            'form_template_path': 'frontend/cmdb/rack_form.html',
-            'template_path': 'frontend/cmdb/rack_list.html',
-            'add_title': '新增机架',
-            'add_action': '/frontend/cmdb/rack_list/rack_add_action/',
-            'edit_title': '编辑机架',
-            'edit_action': '/frontend/cmdb/rack_list/%s/rack_edit_action/',
-            'plugin_name': 'idc_list_plugin',
-            'edit_form_plugin': 'rack_edit_form_plugin',
-            'add_form_plugin': 'rack_add_form_plugin'
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'Rack',
+                'list_url': '/frontend/cmdb/rack_list/',
+                'form_template_path': 'frontend/cmdb/rack_form.html',
+                'template_path': 'frontend/cmdb/rack_list.html',
+                'add_title': '新增机架',
+                'add_action': '/frontend/cmdb/rack_list/rack_add_action/',
+                'edit_title': '编辑机架',
+                'edit_action': '/frontend/cmdb/rack_list/%s/rack_edit_action/',
+                'plugin_name': 'idc_list_plugin',
+                'edit_form_plugin': 'rack_edit_form_plugin',
+                'add_form_plugin': 'rack_add_form_plugin'
+            }}),
 
     # 主机组
     url('^hostgroup_list/', include([
@@ -127,16 +133,17 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'HostGroup',
-            'list_url': '/frontend/cmdb/hostgroup_list/',
-            'form_template_path': 'frontend/common/basic_form.html',
-            'template_path': 'frontend/cmdb/hostgroup_list.html',
-            'add_title': '新增主机组',
-            'add_action': '/frontend/cmdb/hostgroup_list/hostgroup_add_action/',
-            'edit_title': '编辑主机组',
-            'edit_action': '/frontend/cmdb/hostgroup_list/%s/hostgroup_edit_action/',
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'HostGroup',
+                'list_url': '/frontend/cmdb/hostgroup_list/',
+                'form_template_path': 'frontend/common/basic_form.html',
+                'template_path': 'frontend/cmdb/hostgroup_list.html',
+                'add_title': '新增主机组',
+                'add_action': '/frontend/cmdb/hostgroup_list/hostgroup_add_action/',
+                'edit_title': '编辑主机组',
+                'edit_action': '/frontend/cmdb/hostgroup_list/%s/hostgroup_edit_action/',
+            }}),
 
     url(r'host_list/scan_host_job/', scan_host_job),
     # 主机
@@ -149,16 +156,17 @@ urlpatterns = [
         url(r'batch_delete_entity/', simple_batch_delete_entity),
         url(r'$', simple_list),
     ]), {
-            'modulename': 'cmdb.models',
-            'modelname': 'Host',
-            'list_url': '/frontend/cmdb/host_list/',
-            'form_template_path': 'frontend/cmdb/host_form.html',
-            'template_path': 'frontend/cmdb/host_list.html',
-            'add_title': '新增主机',
-            'add_action': '/frontend/cmdb/host_list/host_add_action/',
-            'edit_title': '编辑主机',
-            'edit_action': '/frontend/cmdb/host_list/%s/host_edit_action/',
-            'edit_form_plugin': 'host_edit_form_plugin',
-            'add_form_plugin': 'host_add_form_plugin'
-        }),
+            'args': {
+                'modulename': 'cmdb.models',
+                'modelname': 'Host',
+                'list_url': '/frontend/cmdb/host_list/',
+                'form_template_path': 'frontend/cmdb/host_form.html',
+                'template_path': 'frontend/cmdb/host_list.html',
+                'add_title': '新增主机',
+                'add_action': '/frontend/cmdb/host_list/host_add_action/',
+                'edit_title': '编辑主机',
+                'edit_action': '/frontend/cmdb/host_list/%s/host_edit_action/',
+                'edit_form_plugin': 'host_edit_form_plugin',
+                'add_form_plugin': 'host_add_form_plugin'
+            }}),
 ]
--- common/common_views.py
+++ common/common_views.py
@@ -12,59 +12,40 @@ from common.pageutil import preparePage
 @require_http_methods(["GET"])
 @gzip_page
 @login_required
-def simple_list(request,
-                modulename, modelname, list_url,
-                form_template_path, template_path,
-                add_title, add_action, edit_title, edit_action,
-                plugin_name=None, add_form_plugin=None, edit_form_plugin=None,
-                add_fields='__all__', edit_fields='__all__',
-                ):
+def simple_list(request, args):
     kwargs = dict(filter(lambda x: x[1] != '', request.GET.dict().items()))
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
-    if plugin_name is not None:
-        plugin = getattr(getattr(module, 'c_views'), plugin_name)
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
+    if 'plugin_name' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['plugin_name'])
         plugin_result = plugin()
     obj = instance.objects.filter(**kwargs)
     result_list = preparePage(request, obj)
-    return render(request, template_path, locals(), RequestContext(request))
+    return render(request, args['template_path'], locals(), RequestContext(request))
 
 
 @require_http_methods(["GET"])
 @gzip_page
 @login_required
-def simple_delete_entity(request, pk,
-                         modulename, modelname, list_url,
-                         form_template_path, template_path,
-                         add_title, add_action, edit_title, edit_action, plugin_name=None,
-                         add_form_plugin=None, edit_form_plugin=None,
-                         add_fields='__all__', edit_fields='__all__',
-                         ):
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
-    if plugin_name is not None:
-        plugin = getattr(getattr(module, 'c_views'), plugin_name)
+def simple_delete_entity(request, pk, args):
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
+    if 'plugin_name' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['plugin_name'])
         plugin_result = plugin()
     instance.objects.filter(pk=pk).delete()
-    return redirect(list_url)
+    return redirect(args['list_url'])
 
 
 @require_http_methods(["GET"])
 @gzip_page
 @login_required
-def simple_batch_delete_entity(request,
-                               modulename, modelname, list_url,
-                               form_template_path, template_path,
-                               add_title, add_action,
-                               edit_title,
-                               edit_action, plugin_name=None, add_form_plugin=None,
-                               edit_form_plugin=None, add_fields='__all__', edit_fields='__all__',
-                               ):
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
+def simple_batch_delete_entity(request, args):
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
     ids = request.GET['id'][:-1].split(',')
-    if plugin_name is not None:
-        plugin = getattr(getattr(module, 'c_views'), plugin_name)
+    if 'delete_plugin' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['delete_plugin'])
         plugin_result = plugin()
     instance.objects.filter(pk__in=ids).delete()
     return HttpResponse("")
@@ -73,96 +54,73 @@ def simple_batch_delete_entity(request,
 @require_http_methods(["GET"])
 @gzip_page
 @login_required
-def simple_add(request,
-               modulename, modelname, list_url,
-               form_template_path, template_path,
-               add_title, add_action,
-               edit_title,
-               edit_action, plugin_name=None, add_form_plugin=None, edit_form_plugin=None,
-               add_fields='__all__', edit_fields='__all__',
-               ):
-    action = add_action
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
-    if add_fields == '__all__':
+def simple_add(request, args):
+    action = args['add_action']
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
+    if 'add_fields' not in args:
         form = modelform_factory(instance, fields='__all__')
     else:
-        form = modelform_factory(instance, fields=(add_fields.split(',')))
-    title = add_title
-    if add_form_plugin is not None:
-        plugin = getattr(getattr(module, 'c_views'), add_form_plugin)
+        form = modelform_factory(instance, fields=(args['add_fields'].split(',')))
+    title = args['add_title']
+    if 'add_form_plugin' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['add_form_plugin'])
         plugin_result = plugin(locals())
-    return render(request, form_template_path, locals(), RequestContext(request))
+    return render(request, args['form_template_path'], locals(), RequestContext(request))
 
 
 @require_http_methods(["POST"])
 @gzip_page
 @login_required
-def simple_add_action(request,
-                      modulename, modelname, list_url,
-                      form_template_path, template_path,
-                      add_title, add_action, edit_title, edit_action, plugin_name=None,
-                      add_form_plugin=None, edit_form_plugin=None,
-                      add_fields='__all__', edit_fields='__all__', ):
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
+def simple_add_action(request, args):
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
     form = modelform_factory(instance, fields='__all__')
     form = form(request.POST)
-    if plugin_name is not None:
-        plugin = getattr(getattr(module, 'c_views'), plugin_name)
+    if 'add_action_plugin' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['add_action_plugin'])
         plugin_result = plugin()
     if form.is_valid():
         form.save()
-        return redirect(list_url)
+        return redirect(args['list_url'])
     else:
-        return render(request, form_template_path, locals())
+        return render(request, args['form_template_path'], locals())
 
 
 @require_http_methods(["GET"])
 @gzip_page
 @login_required
-def simple_edit(request, pk,
-                modulename, modelname, list_url,
-                form_template_path, template_path,
-                add_title, add_action, edit_title, edit_action, plugin_name=None,
-                add_form_plugin=None, edit_form_plugin=None,
-                add_fields='__all__', edit_fields='__all__',
-                ):
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
-    if edit_fields == '__all__':
+def simple_edit(request, pk, args):
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
+    if 'edit_fields' not in args:
         form = modelform_factory(instance, fields='__all__')
     else:
-        form = modelform_factory(instance, fields=(edit_fields.split(',')))
-    title = edit_title
-    action = edit_action % pk
+        form = modelform_factory(instance, fields=(args['edit_fields'].split(',')))
+    title = args['edit_title']
+    action = args['edit_action'] % pk
     entity = get_object_or_404(instance, pk=pk)
     form = form(instance=entity)
-    if edit_form_plugin is not None:
-        plugin = getattr(getattr(module, 'c_views'), edit_form_plugin)
+    if 'edit_form_plugin' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['edit_form_plugin'])
         plugin_result = plugin(locals())
-    return render(request, form_template_path, locals())
+    return render(request, args['form_template_path'], locals())
 
 
 @require_http_methods(["POST"])
 @gzip_page
 @login_required
-def simple_edit_action(request, pk,
-                       modulename, modelname, list_url,
-                       form_template_path, template_path,
-                       add_title, add_action, edit_title, edit_action, plugin_name=None,
-                       add_form_plugin=None, edit_form_plugin=None,
-                       add_fields='__all__', edit_fields='__all__', ):
-    module = __import__(modulename)
-    instance = getattr(getattr(module, 'models'), modelname)
+def simple_edit_action(request, pk, args):
+    module = __import__(args['modulename'])
+    instance = getattr(getattr(module, 'models'), args['modelname'])
     entity = get_object_or_404(instance, pk=pk)
     form = modelform_factory(instance, fields='__all__')
     form = form(request.POST, instance=entity)
-    if plugin_name is not None:
-        plugin = getattr(getattr(module, 'c_views'), plugin_name)
+    if 'edit_action_plugin' in args:
+        plugin = getattr(getattr(module, 'c_views'), args['edit_action_plugin'])
         plugin_result = plugin()
     if form.is_valid():
         form.save()
-        return redirect(list_url)
+        return redirect(args['list_url'])
     else:
-        return render(request, form_template_path, locals())
+        return render(request, args['form_template_path'], locals())
