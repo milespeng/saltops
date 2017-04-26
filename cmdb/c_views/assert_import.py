84b8d1a82444011c887e21788ea6fc921a51efd5
kira <529280602@qq.com>
2017-04-26 22:14:26 +0800

    资产导入

--- cmdb/c_views/assert_import.py
+++ cmdb/c_views/assert_import.py
@@ -1,12 +1,18 @@
+import os
+from cmdb.models import *
 from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
 from django.forms import ModelForm, modelform_factory
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template import RequestContext
 from django.views.decorators.gzip import gzip_page
 from django.views.decorators.http import require_http_methods
-
+import xlrd
 from common.pageutil import preparePage
+from django.core.files.storage import default_storage
+
+from saltops import settings
 
 
 @require_http_methods(["GET"])
@@ -20,6 +26,18 @@ def assert_import_index(request):
 @gzip_page
 @login_required
 def upload_file(request):
-    myFile = request.FILES.get("file", None)  # 获取上传的文件，如果没有文件，则默认为None
-    if not myFile:
-        return HttpResponse("no files for upload!")
+    assert_file = request.FILES.get("file", None)
+    wb = xlrd.open_workbook(filename=None, file_contents=assert_file.read())
+    # 读取机房等级信息
+    idc_level_table = wb.sheets()[0]
+    for i in range(1, idc_level_table.nrows):
+        row = idc_level_table.row_values(i)
+        if len(IDCLevel.objects.filter(name=row[0])) == 0:
+            IDCLevel(name=row[0], comment=row[1]).save()
+
+    # 读取ISP信息
+    isp_table = wb.sheets()[1]
+    for i in range(1, isp_table.nrows):
+        row = isp_table.row_values(i)
+        if len(ISP.objects.filter(name=row[0])) == 0:
+            ISP(name=row[0]).save()
