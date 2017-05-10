51bb8ce14f229e6a91fc1bce11a8b0cf3a4fad7b
kira <529280602@qq.com>
2017-05-10 22:11:17 +0800

    业务

--- deploy_manager/urls.py
+++ deploy_manager/urls.py
@@ -5,7 +5,7 @@ from common import views
 from common.common_views import *
 
 urlpatterns = [
-    # 机房等级
+    # 业务模块
     url(r'^project_module_list/', include([
         url(r'(?P<pk>\d+)/project_module_edit_action/', simple_edit_action),
         url(r'(?P<pk>\d+)/project_module_edit/', simple_edit),
@@ -29,4 +29,27 @@ urlpatterns = [
             }
         }),
 
+    # 业务
+    url(r'^project_list/', include([
+        url(r'(?P<pk>\d+)/project_edit_action/', simple_edit_action),
+        url(r'(?P<pk>\d+)/project_edit/', simple_edit),
+        url(r'project_add_action/', simple_add_action),
+        url(r'project_add/', simple_add),
+        url(r'(?P<pk>\d+)/delete_entity/', simple_delete_entity),
+        url(r'batch_delete_entity/', simple_batch_delete_entity),
+        url(r'$', simple_list),
+    ]), {
+            'args': {
+                'modulename': 'deploy_manager.models',
+                'modelname': 'Project',
+                'list_url': '/frontend/deploy_manager/project_list/',
+                'form_template_path': 'frontend/deploy_manager/project_form.html',
+                'template_path': 'frontend/deploy_manager/project_list.html',
+                'add_title': '新增业务',
+                'add_action': '/frontend/deploy_manager/project_list/project_add_action/',
+                'edit_title': '编辑业务',
+                'edit_action': '/frontend/deploy_manager/project_list/%s/project_edit_action/',
+                # 'plugin_name': 'project_list_plugin'
+            }
+        }),
 ]
