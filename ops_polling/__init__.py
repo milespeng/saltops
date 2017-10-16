c23b4ca76fac11093afb417de6f452cdfa15d1ee
kira <you@example.com>
2017-10-16 11:24:14 +0800

    巡检

---
+++ ops_polling/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'ops_polling.apps.OpsPollingConfig'
---
+++ ops_polling/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpsPollingConfig(AppConfig):
+    name = 'ops_polling'
+    verbose='巡检作业'
